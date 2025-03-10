54a69cc0a0d308729fb893ab8fcd64f674ca1a97
YulianaSanmartin <yulianasanmartin1124627@correo.itm.edu.co>
2025-03-09 20:02:33 -0500

    Add files via upload

---
+++ Circle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TallerHerencia
+{
+    class Circle : GeometricFigure
+    {
+        private double _r;
+        public Circle(string name, double r) : base(name) => R = r;
+        public double R
+        {
+            get => _r;
+            set
+            {
+                if (value <= 0) throw new ArgumentOutOfRangeException("El radio debe ser mayor que 0.");
+                _r = value;
+            }
+        }
+        public override double GetArea() => Math.PI * Math.Pow(R, 2);
+        public override double GetPerimeter() => 2 * Math.PI * R;
+        private void ValidateR(double value)
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentOutOfRangeException("El radio debe ser mayor que 0.");
+            }
+        }
+   
+    }
+}
+
---
+++ Trapeze.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TallerHerencia
+{
+    class Trapeze : Triangle
+
+    {
+        private double _d;
+        public Trapeze(string name, double a, double b, double c, double d, double h) : base(name, a, b, c, h) => D = d;
+        public double D
+        {
+            get => _d;
+            set
+            {
+                if (value <= 0) throw new ArgumentOutOfRangeException("El lado D debe ser mayor que 0.");
+                _d = value;
+            }
+        }
+        public override double GetArea() => (B + D) * H / 2;
+        public override double GetPerimeter() => A + B + C + D;
+
+        private double ValidateD(double value)
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentOutOfRangeException("El lado D debe ser mayor que 0.");
+            }
+            return value;
+        }
+    }
+}
+  
