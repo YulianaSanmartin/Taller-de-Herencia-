e7160c65c647b18d7d8f3ba573dcfc4ccbb6ae5e
YulianaSanmartin <yulianasanmartin1124627@correo.itm.edu.co>
2025-03-09 20:05:38 -0500

    Add files via upload

---
+++ GEOMETRY/GeometricFigure.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace TallerHerencia
+{
+    public abstract class GeometricFigure
+
+    {
+        public string Name { get; set; }
+        public GeometricFigure(string name) => Name = name;
+        public abstract double GetArea();
+        public abstract double GetPerimeter();
+        public override string ToString()
+        {
+            return $"{Name} => Area.....: {GetArea():F5}  Perimeter: {GetPerimeter():F5}";
+        }
+
+    }
+}
